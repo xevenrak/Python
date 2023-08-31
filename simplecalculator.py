1f6fa34aef60b2eeae893b2b25d3084c5ce2cef5
xevenrak <143601455+xevenrak@users.noreply.github.com>
2023-08-31 15:08:50 +0700

    Update simplecalculator.py

--- simplecalculator.py
+++ simplecalculator.py
@@ -1,31 +1,25 @@
-# ngambil angka ke1 trus disimpen di num1
+
 num1 = float(input("Enter the first number: "))
 
-# ngambil angka ke2 trus disimpen ke num2
 num2 = float(input("Enter the second number: "))
 
-# ngambil hasil operasi dari user trus disimpen di op
 op = input("Enter an operation (+, -, *, /): ")
 
-# hasill
 result = 0
 
-# ngecek operasi
-if op == "+": # kalo op +
-    # ditambah
+if op == "+": 
     result = num1 + num2
-elif op == "-": # kalo op -
-    # dikurangi
+
+elif op == "-": 
     result = num1 - num2
-elif op == "*": # kalo op *
-    # dikali
+
+elif op == "*": 
     result = num1 * num2
-elif op == "/": # kalo op /
-    # dibagi
+
+elif op == "/": 
     result = num1 / num2
-else: # kalo op hal lain
-    # pesan error
+
+else: 
     print("Invalid operation")
 
-# hasil
 print(num1, op, num2, "=", result)
